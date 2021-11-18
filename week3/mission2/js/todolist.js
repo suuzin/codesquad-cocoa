3a78a650f23aec9851a561f76eb17f64cd7a9efa
suuzin <8oozin@gmail.com>
2021-11-18 13:12:17 +0900

    함수로구현

--- week3/mission2/js/todolist.js
+++ week3/mission2/js/todolist.js
@@ -5,12 +5,10 @@ function register(){
     let input = document.createElement("input"); 
     input.setAttribute('type','checkbox');
     
-    //let checkedbox = document.createElement("p");
-    //checkedbox.setAttribute("class","checked");
     addP.appendChild(input);
 
     let newText = document.createTextNode(doIt.value); 
-    addP.appendChild(newText); 
+    addP.appendChild(newText);
 
     let delBtn = document.createElement("span");
     let delText = document.createTextNode("X");
@@ -19,24 +17,18 @@ function register(){
     addP.appendChild(delBtn);
     
     let allList = document.querySelector("#allList");
-    allList.appendChild(addP);
+    allList.appendChild(addP); 
     doIt.value =""; 
 
-    //삭제기능
     let removeBtn = document.querySelectorAll(".del");
-    //x버튼 눌렀을 때 뭘 삭제 해야 하나?-노드삭제는 부모노드에서 한다.
     for(let i = 0; i < removeBtn.length; i++){ 
-        removeBtn[i].addEventListener("click",function(){
-            if(this.parentNode.parentNode)
+        removeBtn[i].addEventListener("click",function(){ 
+            if(this.parentNode.parentNode) 
                 this.parentNode.parentNode.removeChild(this.parentNode); 
         });
     }
 
-    //다시 구현해야됨
-    let allP = document.querySelectorAll("p");
-    for(let i = 0; i < allP.length; i++) {
-       allP[i].addEventListener("click",function(ev){
-        ev.target.classList.toggle("checked");
-       });
-    }
+    input.addEventListener("click",function(ev){
+        ev.target.parentNode.classList.toggle('clicked');
+    });
 }
