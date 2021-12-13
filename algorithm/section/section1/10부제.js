e3653281318171573047c094c89de46bd24434b0
suuzin <93182666+suuzin@users.noreply.github.com>
2021-12-13 23:05:31 +0900

    Delete 10부제.js

--- algorithm/section/section1/10부제.js
+++
@@ -1,9 +0,0 @@
-function solution(day,arr){
-    let count = 0;
-    arr.forEach(element => {
-       if(element%10 ===day) count++; 
-    });
-    return count;
-}
-let arr = [25, 23, 11, 47, 53, 17, 33];
-console.log(solution(3,arr));
